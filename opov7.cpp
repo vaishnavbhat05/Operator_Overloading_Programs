d5393fd66e46f8d9803f92f38461cf2ad906a6b8
vaishnavbhat05 <104012712+vaishnavbhat05@users.noreply.github.com>
2022-04-20 23:26:29 +0530

    Create opov7.cpp

---
+++ opov7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+
+private:
+
+    int num, den;
+
+public:
+
+    Fraction(int n, int d) { num = n; den = d; }
+ 
+
+    // Conversion operator: return float value of fraction
+
+    operator float() const {
+
+        return float(num) / float(den);
+
+    }
+};
+ 
+
+int main() {
+
+    Fraction f(2, 5);
+
+    float val = f;
+
+    cout << val << '\n';
+
+    return 0;
+}
